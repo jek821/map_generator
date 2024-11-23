65e893bb9eda778b1aefbf0660dd9082f97d6f22
Jacob <jacobkriss2001@gmail.com>
2024-11-22 20:37:15 -0500

    push

---
+++ backend/config/db.js
@@ -0,0 +1,28 @@
+require('dotenv').config(); // Load .env variables
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+// Use the MONGO_URI from the .env file
+const uri = process.env.MONGO_URI;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+
+run().catch(console.dir);
